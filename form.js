d4be83e98b0d91ea80ed01a1fad132cdc182d41a
pranav <prnvkatkar88@gmail.com>
2021-12-01 19:36:01 +0530

    Added:fuctionality to tell user to add valid first name

---
+++ form.js
@@ -0,0 +1,10 @@
+//UC1 user should enter valid first name
+const text = document.querySelector('#text');
+const textError = document.querySelector('.text-error');
+text.addEventListener('input',function(){
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if (nameRegex.test(text.value))
+        textError.textContent="";
+    else textError.textContent = "Name is incorrect";
+});
+
